1300305fb8162f38358241bb16210225627e218f
Tina <longevity.tina@gmail.com>
2020-04-09 17:17:13 -0700

    Adds about page method

--- wasteland/main_app/views.py
+++ wasteland/main_app/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
-  return HttpResponse('<h1>CV-WASTELAND /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>CV-WASTELAND /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+def about(request):
+    return render(request, 'about.html')

ff271ce87998b979750d758d1889f110217e2dae
Tina <longevity.tina@gmail.com>
2020-04-11 17:02:15 -0700

    Initializes new character form

--- wasteland/main_app/forms.py
+++ wasteland/main_app/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Item
+from .models import Item, Character
 
 
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('name', 'description')
+
+
+class CharacterForm(forms.ModelForm):
+    class Meta:
+        model = Character
+        fields = ('name', 'location', 'age',
+                  'starting_date', 'occupation',
+                  'intellect', 'toughness', 'speed',
+                  'stamina', 'luck', 'strength', 'items', )
+        # Todo add in the rest of fields

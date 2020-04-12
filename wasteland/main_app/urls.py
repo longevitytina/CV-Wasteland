70851e33f630616488de956dba2bb081a28be0f7
Tina <longevity.tina@gmail.com>
2020-04-11 17:02:56 -0700

    Adds new character route

--- wasteland/main_app/urls.py
+++ wasteland/main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
+    path('profile/new', views.new_character, name='new_character'),
     path('items/', views.items_list, name='items_list'),
     path('items/<int:item_id>/edit', views.item_edit, name='item_edit'),
     path('profile/<int:character_id>/',

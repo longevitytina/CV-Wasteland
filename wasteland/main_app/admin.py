925dd1768cfba0e0e7ca1e67f54502d2c8d42e9d
Tina <longevity.tina@gmail.com>
2020-04-12 11:04:58 -0700

    model changes

--- wasteland/main_app/admin.py
+++ wasteland/main_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Character, Item
+from .models import Character, Item, Situation, ItemAction, Reaction, Log
 
 admin.site.register(Character)
 admin.site.register(Item)
+admin.site.register(Situation)
+admin.site.register(ItemAction)
+admin.site.register(Reaction)
+admin.site.register(Log)
+
 # Register your models here.
--- wasteland/main_app/models.py
+++ wasteland/main_app/models.py
@@ -69,6 +69,7 @@ class Character(models.Model):
 class Situation(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=100)
+    # response = models.
 
     def __str__(self):
         return self.name
